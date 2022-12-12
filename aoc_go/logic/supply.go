0acd9e00baad70cf6a4f400f185250716336df0a
kalifun <kalifun@163.com>
2022-12-12 18:51:28 +0800

    Update supply.go
    day5 part2

--- aoc_go/logic/supply.go
+++ aoc_go/logic/supply.go
@@ -62,19 +62,22 @@ func (s *supplyStacks) collect() {
 		}
 		c.AutoWork(string(line))
 	}
-	s.answer1 = c.GetPart1Answer()
+	s.answer1 = getAnswer(c.part1Stacks)
+	s.answer2 = getAnswer(c.part2Stacks)
 }
 
 // crates  TODO
 type crates struct {
-	stacks map[uint][]string
+	part1Stacks map[uint][]string
+	part2Stacks map[uint][]string
 }
 
 // NewCrates
 //  @return *crates
 func NewCrates() *crates {
 	return &crates{
-		stacks: make(map[uint][]string),
+		part1Stacks: make(map[uint][]string),
+		part2Stacks: make(map[uint][]string),
 	}
 }
 
@@ -117,10 +120,16 @@ func (c *crates) AutoWork(line string) {
 			// 说明满足
 			word := string(line[i+1])
 			site := (i / 4) + 1
-			if v, ok := c.stacks[uint(site)]; ok {
-				c.stacks[uint(site)] = append(v, word)
+			if v, ok := c.part1Stacks[uint(site)]; ok {
+				c.part1Stacks[uint(site)] = append(v, word)
 			} else {
-				c.stacks[uint(site)] = []string{word}
+				c.part1Stacks[uint(site)] = []string{word}
+			}
+
+			if v, ok := c.part2Stacks[uint(site)]; ok {
+				c.part2Stacks[uint(site)] = append(v, word)
+			} else {
+				c.part2Stacks[uint(site)] = []string{word}
 			}
 		}
 	}
@@ -132,29 +141,65 @@ func (c *crates) AutoWork(line string) {
 //  @param to
 //  @param num
 func (c *crates) move(from, to, num uint) {
-	// 取出要move的内容
-	var waitMoveList []string
-	if fromVal, ok := c.stacks[from]; ok {
-		waitMoveList = c.stacks[from][:num]
-		c.stacks[from] = fromVal[num:]
-	}
+	dm := NewDataMigration(from, to, num)
+	part1 := dm.Migration(c.part1Stacks, true)
+	c.part1Stacks = part1
+	part2 := dm.Migration(c.part2Stacks, false)
+	c.part2Stacks = part2
+}
 
-	utils.Reverse(waitMoveList)
-	if _, ok := c.stacks[to]; ok {
-		slice := make([]string, len(waitMoveList)+len(c.stacks[to]))
-		copy(slice, waitMoveList)
-		copy(slice[len(waitMoveList):], c.stacks[to])
-		c.stacks[to] = slice
+// dataMigration  TODO
+type dataMigration struct {
+	from uint
+	to   uint
+	num  uint
+}
+
+// NewDataMigration
+//  @param from
+//  @param to
+//  @param num
+//  @return dataMigration
+func NewDataMigration(from, to, num uint) dataMigration {
+	return dataMigration{
+		from: from,
+		to:   to,
+		num:  num,
 	}
 }
 
-// GetPart1Answer
-//  @receiver c
+// Migration
+//  @receiver d
+//  @param stack
+//  @param reversal
+//  @return map
+func (d dataMigration) Migration(stack map[uint][]string, reversal bool) map[uint][]string {
+	var waitMoveList []string
+	if fromVal, ok := stack[d.from]; ok {
+		waitMoveList = stack[d.from][:d.num]
+		stack[d.from] = fromVal[d.num:]
+	}
+
+	if reversal {
+		utils.Reverse(waitMoveList)
+	}
+	if _, ok := stack[d.to]; ok {
+		slice := make([]string, len(waitMoveList)+len(stack[d.to]))
+		copy(slice, waitMoveList)
+		copy(slice[len(waitMoveList):], stack[d.to])
+		stack[d.to] = slice
+	}
+
+	return stack
+}
+
+// getAnswer
+//  @param stack
 //  @return interface{}
-func (c *crates) GetPart1Answer() interface{} {
+func getAnswer(stack map[uint][]string) interface{} {
 	var res []string
-	for i := 0; i < len(c.stacks); i++ {
-		if v, ok := c.stacks[uint(i+1)]; ok {
+	for i := 0; i < len(stack); i++ {
+		if v, ok := stack[uint(i+1)]; ok {
 			if len(v) != 0 {
 				res = append(res, v[0])
 			}
